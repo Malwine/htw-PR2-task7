a8c0fa3e9a34def8d1c5659192114ef171189dab
Malwine <malwine@mailbox.org>
2015-06-16 17:47:11 +0200

    Add slider

--- src/ss2015_aufgabe7/PIGui.java
+++ src/ss2015_aufgabe7/PIGui.java
@@ -1,11 +1,20 @@
 package ss2015_aufgabe7;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class PIGui extends JFrame {
+public class PIGui extends JFrame implements ActionListener, WindowListener, ChangeListener{
 	
 	private class PaintCanvas extends JPanel{
 		
@@ -15,19 +24,96 @@ public class PIGui extends JFrame {
 			g.drawArc(20, 20, 800, 800, 90, 90);
 		}
 	}
+	
+	static final int P_MIN = 0;
+    static final int P_MAX = 30;
+    static final int P_INIT = 15;
 
 	PIGui(){
 		super();
 		this.setTitle("Calculation of PI");
-		getContentPane().add( new PaintCanvas(), BorderLayout.CENTER);
+		getContentPane().add( new PaintCanvas(), BorderLayout.NORTH);
 		
+		JLabel sliderLabel = new JLabel("pixel", JLabel.CENTER);
+	    sliderLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    
+	    JSlider pixel = initJSlider();
+	    
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(460,600);
+		this.add(sliderLabel);
+	    this.add(pixel);
+		this.setSize(460,700);
 		this.setVisible(true);
+	}
+	
+	private JSlider initJSlider(){
+		JSlider pixel = new JSlider(JSlider.HORIZONTAL, P_MIN, P_MAX, P_INIT);
+	    pixel.addChangeListener(this);
+	    pixel.setMajorTickSpacing(10);
+        pixel.setMinorTickSpacing(1);
+        pixel.setPaintTicks(true);
+        pixel.setPaintLabels(true);
+        pixel.setBorder(BorderFactory.createEmptyBorder(0,0,10,0));
+        pixel.setFont(new Font("Serif", Font.ITALIC, 15));
+        return pixel;
 	}
 	
 	public static void main(String[] args) {
 		new PIGui();
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
