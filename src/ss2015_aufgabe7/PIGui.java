22fca8d4ce8cdbaf35089c6166e38a68fd01c6f3
Malwine <malwine@mailbox.org>
2015-06-19 22:01:04 +0200

    Add labels

--- src/ss2015_aufgabe7/PIGui.java
+++ src/ss2015_aufgabe7/PIGui.java
@@ -11,10 +11,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class PIGui extends JFrame implements ActionListener, WindowListener, ChangeListener{
+	JPanel southPanel;
+	JSlider slider;
 	
 	private class PaintCanvas extends JPanel{
 		
@@ -24,28 +27,52 @@ public class PIGui extends JFrame implements ActionListener, WindowListener, Cha
 			g.drawArc(20, 20, 800, 800, 90, 90);
 		}
 	}
-	
-	static final int P_MIN = 0;
-    static final int P_MAX = 30;
-    static final int P_INIT = 15;
 
 	PIGui(){
-		super();
-		this.setTitle("Calculation of PI");
+		super("Calculation of PI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		getContentPane().add( new PaintCanvas(), BorderLayout.CENTER);
 	    
-	    JSlider pixel = initJSlider();
-	    
+		southPanel = initSouthPanel();
+		this.add(southPanel, BorderLayout.SOUTH);
 		
-	    this.add(pixel, BorderLayout.SOUTH);
-		this.setSize(460,700);
+	    this.setSize(460,700);
 		this.setVisible(true);
 	}
 	
+	private JPanel initSouthPanel() {
+		
+		JPanel panel = new JPanel(new BorderLayout()); // HINT: von Flowlayout zu BorderLayout 
+		
+		//add control elements
+		slider =  initJSlider();
+		slider.addChangeListener(this);
+		panel.add(slider, BorderLayout.NORTH);
+		
+		JPanel panelCenter = new JPanel(new GridLayout(3,2));
+		panel.add(panelCenter, BorderLayout.CENTER);
+		
+		JLabel label1 = new JLabel("Anzahl Punkte:");
+		label1.setBorder(new EmptyBorder(10,10,10,10));
+		panelCenter.add(label1);
+		
+		JLabel label2 = new JLabel("Anzahl rote Punkte:");
+		label2.setBorder(new EmptyBorder(10,10,10,10));
+		panelCenter.add(label2);
+		
+		JLabel label3 = new JLabel("Anzahl blaue Punkte:");
+		label3.setBorder(new EmptyBorder(10,10,10,10));
+		panelCenter.add(label3);
+		
+		JLabel labelSouth = new JLabel("labelSouth");
+		panel.add(labelSouth, BorderLayout.SOUTH);
+		
+		return panel;
+	}
+
 	private JSlider initJSlider(){
-		JSlider pixel = new JSlider(JSlider.HORIZONTAL, P_MIN, P_MAX, P_INIT);
+		JSlider pixel = new JSlider(JSlider.HORIZONTAL, 0, 30, 15);
 	    pixel.addChangeListener(this);
 	    pixel.setMajorTickSpacing(10);
         pixel.setMinorTickSpacing(1);
